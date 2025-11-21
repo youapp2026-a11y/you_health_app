fbff90ea4d7aab0587f24d9770d39e07d9fe8529
youapp2026 <youapp2026@gmail.com>
2025-11-21 14:07:13 +0000

    feat: Configure Supabase keys and add google_mobile_ads

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,7 @@
+
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const String supabaseUrl = 'YOUR_SUPABASE_URL';
-const String supabaseAnonKey = 'YOUR_SUPABASE_ANNON_KEY';
+const String supabaseUrl = 'https://cmflvybwoutunmyritob.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtZmx2eWJ3b3V0dW5teXJpdG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3Mjk5NTksImV4cCI6MjA3OTMwNTk1OX0.PdFDf5oYEWqMex2FXZV09AhNoagUrQjleuVVQba_TfE';
 
 final supabase = Supabase.instance.client;
